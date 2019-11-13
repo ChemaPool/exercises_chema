a5e1e308045b13f7cdcaac670b72fbb806fea6a3
ChemaPool <ChemaPool>
2019-11-13 16:10:08 -0600

    staircase_review_clean

--- staircase.rb
+++ staircase.rb
@@ -5,7 +5,7 @@ require 'stringio'
 
 # Complete the staircase function below.
 def staircase(n)
-  (1..n).step() do |number_figure|
+  (1..n).step do |number_figure|
     puts "#{' ' * (n - number_figure)}" "#{ ('#' * number_figure)}"
   end
 end
