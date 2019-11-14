9f312c24cd7ed9488cd5bf3d3de51cbcdfa5ab58
ChemaPool <ChemaPool>
2019-11-14 17:34:39 -0600

    clean_code_2_use_guard_clause_review_4

--- clean_code_2.rb
+++ clean_code_2.rb
@@ -2,5 +2,6 @@ def longname
   name = fullname
   return name unless organization_id
   organization = Organization.lookup(id: organization_id)
-  return name += " (#{organization.name})" unless organization
+  return name += " (#{organization.name})" if organization
+  name
 end
