3e42e28f8daf72536448bc102ac761fca5f6ec72
ChemaPool <ChemaPool>
2019-11-14 14:41:43 -0600

    exercise_8_clean_code

--- clean_code_1.rb
+++ clean_code_1.rb
@@ -1,52 +1,44 @@
 def customer_state(session_id = nil)
-  return unless !Setting.get('chat')
-  { state: 'chat_disabled' }
+  return { state: 'chat_disabled' } unless Setting.get('chat')
 
-  if session_id
-    chat_session = Chat::Session.find_by(session_id: session_id, state: %w[waiting running])
+  return unless session_id
+  chat_session = Chat::Session.find_by(session_id: session_id, state: %w[waiting running])
+  return unless chat_session
 
-    if chat_session
-      return unless chat_session.state == 'running'
-      user = nil
+  chat_session_running()
 
-      return user_chat_session()
-      return user_image()
-      user = {
-        name:   chat_user.fullname,
-        avatar: url,
-      }
-
-      session = Chat::Session.messages_by_session_id(session_id)
-      return unless session?
-      { state: 'reconnect', session: session, agent: user, }
-
-      elsif chat_session.state == 'waiting'
-        return {
-          state:    'reconnect',
-          position: chat_session.position,
-        }
-      end
-      
-    end
-  end
-
-  return unless Chat.active_agent_count([id]).zero?
-  { state: 'offline' }
-
+  return {state: 'offline'} if Chat.active_agent_count([id]).zero?
   waiting_count = Chat.waiting_chat_count(id)
-  return unless waiting_count >= max_queue
-  {state: 'no_seats_available', queue: waiting_count, }
+  return {state: 'no_seats_available', queue: waiting_count, } if waiting_count >= max_queue
 
   { state: 'online' }
+end
+
+def chat_session_running()
+  return if chat_session.state == 'running'
+    user_chat_session()
+
+    user = {
+      name:   chat_user.fullname,
+      avatar: url,
+    }
+
+    session = Chat::Session.messages_by_session_id(session_id)
+    return { state: 'reconnect', session: session, agent: user} if session
+    return { state: 'reconnect', position: chat_session.position } unless chat_session.state == 'waiting'
+  
 end
 
 def user_chat_session()
   if chat_session.user_id
     chat_user = User.lookup(id: chat_session.user_id)
     url = nil
+    user_image()
+  end
 end
 
 def user_image()
   if chat_user.image && chat_user.image != 'none'
     url = "#{Setting.get('http_type')}://#{Setting.get('fqdn')}/api/v1/users/image/#{chat_user.image}"
+  end
 end
