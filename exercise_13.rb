b612f6ad0a3f376dcd1333683a2dcd0fe48a5cf6
ChemaPool <ChemaPool>
2019-11-19 17:31:50 -0600

    exercise_13_change_method_all

--- exercise_13.rb
+++ exercise_13.rb
@@ -1,30 +1,22 @@
 class Contact
   def self.all
-    first_contact = {:age => 20, :first_name => "Jose", :last_name => "Lopez", :genre => :M}
-    second_contact = {:age => 21, :first_name => "Oscar", :last_name => "Perez", :genre => :M}
-    third_contact = {:age => 22, :first_name => "Martha", :last_name => "Sanchez", :genre => :F}
-
-    contacts = []
-    contacts.push(first_contact, second_contact, third_contact)
-    contacts
+    @contacts = [
+      { age: 20, first_name: "Jose", last_name: "Lopez", genre: :M},
+      {age: 21, first_name: "Oscar", last_name: "Perez", genre: :M},
+      {age: 22, first_name: "Martha", last_name: "Sanchez", genre: :F}
+    ]
+    @contacts
   end
   def self.find_by(key, value)
-    first_contact = {:age => 20, :first_name => "Jose", :last_name => "Lopez", :genre => :M}
-    second_contact = {:age => 21, :first_name => "Oscar", :last_name => "Perez", :genre => :M}
-    third_contact = {:age => 22, :first_name => "Martha", :last_name => "Sanchez", :genre => :F}
-
-    contacts = []
-    contacts.push(first_contact, second_contact, third_contact)
-
-    contacts.each do |value_contact|
-      next if value_contact[key] != value
-        puts "Edad: #{value_contact[:age]}"
-        puts "Nombre: #{value_contact[:first_name]}"
-        puts "Apellido: #{value_contact[:last_name]}"
-        puts "Genero: #{value_contact[:genre]}"
+    @contacts.each do |value_contact|
+      next unless value_contact[key].eql?(value)
+        puts "Age: #{value_contact[:age]}"
+        puts "Firts Name: #{value_contact[:first_name]}"
+        puts "Last Name: #{value_contact[:last_name]}"
+        puts "Genre: #{value_contact[:genre]}"
     end
   end
 end
 
 Contact.all
-Contact.find_by(:age, 20)
+Contact.find_by(:last_name, "Sanchez")
