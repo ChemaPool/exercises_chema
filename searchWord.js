b081cd05484757d7b59c5c9831f83c6a831ec140
ChemaPool <ChemaPool>
2019-11-22 13:40:42 -0600

    exercise_22_js_searchWord

---
+++ searchWord.js
@@ -0,0 +1,16 @@
+const peopleNames = ["Pedro", "Maria", "Rosa"]
+const animals = ["Caballo", "Oso", "Perro"]
+const thing = ["Petroleo", "Carne", "Rosca"]
+const names = peopleNames.concat(animals, thing);
+
+const searchWord = term => {
+  const wordsFound = []
+  names.forEach(word => {
+    if (word.toLowerCase().includes(term.toLowerCase())){
+      wordsFound.push(word.toLowerCase());
+    }
+  });
+  return wordsFound;
+}
+
+searchWord('pE');
