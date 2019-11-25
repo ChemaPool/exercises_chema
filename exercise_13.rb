e7e02a58643239d3f8dd1d87f24c184efe539776
ChemaPool <57272550+ChemaPool@users.noreply.github.com>
2019-11-24 21:45:10 -0600

    change method all and use 'detect' to find contact

--- exercise_13.rb
+++ exercise_13.rb
@@ -1,21 +1,16 @@
 class Contact
   def self.all
-    @contacts = [
+    [
       { age: 20, first_name: "Jose", last_name: "Lopez", genre: :M},
-      {age: 21, first_name: "Oscar", last_name: "Perez", genre: :M},
-      {age: 22, first_name: "Martha", last_name: "Sanchez", genre: :F}
+      { age: 21, first_name: "Oscar", last_name: "Perez", genre: :M},
+      { age: 22, first_name: "Martha", last_name: "Sanchez", genre: :F}
     ]
-    @contacts
   end
+
   def self.find_by(key, value)
-    contact = []
-    @contacts.each do |value_contact|
-      next unless value_contact[key].eql?(value)
-       contact.push(value_contact[:age], value_contact[:first_name], value_contact[:last_name], value_contact[:genre])
-    end
-    contact
+    all.detect { |contact| return contact if contact[key] == value }
+    puts "No matches"
   end
 end
 
-Contact.all
-Contact.find_by(:last_name, "Lopez")
+Contact.find_by(:first_name, "Martha")
