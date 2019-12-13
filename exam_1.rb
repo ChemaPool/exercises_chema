d8d1e0289007b6cc277d7247c556d0acee8fb374
ChemaPool <57272550+ChemaPool@users.noreply.github.com>
2019-12-12 23:26:21 -0600

    change case by if

--- exam_1.rb
+++ exam_1.rb
@@ -1,12 +1,10 @@
 def find_number(list_numbers, score)
   list_numbers.each do |number, text_message|
-    case
-      when score <= number then return "#{text_message}"
-    end
+    return "#{text_message}" if score < number
   end
 end
 
 list_numbers = { 0 => "Sin fico", 10 => "Con poco fico", 20 => "Con fico regular" }
-score = 4
+score = 10
 
 find_number(list_numbers, score)
